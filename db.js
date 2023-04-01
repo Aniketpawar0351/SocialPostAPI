930a0d0fe5f3a775069a55be1d82836c54f8c939
Aniket <aniketpawar0351@gmail.com>
2023-04-01 13:30:06 +0530

    new endpoint added

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 //const MONGODB_URI = 'mongodb://localhost:27017/myapp';
 const MONGODB_URI = 'mongodb+srv://cn_grp3:cn_grp3@cluster0.bf5bawq.mongodb.net/?retryWrites=true&w=majority';
 
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
